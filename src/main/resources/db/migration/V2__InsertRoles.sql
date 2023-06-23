70c9f6513096b307ac07465727c6b9fd49e653b3
Ankit Mishra <ankitm17.2001@gmail.com>
2023-06-24 02:23:45 +0530

    Emptied the migration script

--- src/main/resources/db/migration/V2__InsertRoles.sql
+++ src/main/resources/db/migration/V2__InsertRoles.sql
@@ -1,11 +1,11 @@
--- Create ems_role table
-CREATE TABLE ems_role (
-  id INT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL
-);
-
--- Insert initial data into ems_role table
-INSERT INTO ems_role (id, name) VALUES (1, 'ADMIN_ROLE');
-INSERT INTO ems_role (id, name) VALUES (2, 'EMPLOYEE_ROLE');
-
--- Add additional tables and schema changes as needed
+---- Create ems_role table
+--CREATE TABLE ems_role (
+--  id INT PRIMARY KEY,
+--  name VARCHAR(50) NOT NULL
+--);
+--
+---- Insert initial data into ems_role table
+--INSERT INTO ems_role (id, name) VALUES (1, 'ADMIN_ROLE');
+--INSERT INTO ems_role (id, name) VALUES (2, 'EMPLOYEE_ROLE');
+--
+---- Add additional tables and schema changes as needed
